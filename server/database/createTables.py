b48028a860ef32d00ff21fc73013408458685b8e
henryg0 <hgip@uci.edu>
2020-10-10 16:10:15 -0700

    fixed database

--- server/database/createTables.py
+++ server/database/createTables.py
@@ -2,14 +2,15 @@ from database import session
 
 """session.execute("LIST ALL of Calhackeronis")
 session.execute("ALTER USER Calhackeronis SUPERUSER")"""
-session.execute("CREATE KEYSPACE IF NOT EXISTS Todo WITH REPLICATION = { 'class' : 'hello'}")
+
+# session.execute("CREATE KEYSPACE IF NOT EXISTS Todo WITH REPLICATION = { 'class' : 'hello'}")
 session.execute("USE todo")
 
-query = "CREATE TABLE UserProfile( {}, {}, {}, {} )".format(
+query = "CREATE TABLE IF NOT EXISTS todo.userProfile( {}, {}, {}, {} )".format(
     "id uuid PRIMARY KEY", 
     "firstName text", 
     "lastName text", 
     "email text"
 )
 
-print (session.execute(query))
+print (session.execute(query).one())
