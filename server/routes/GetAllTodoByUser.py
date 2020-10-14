f953bed5189caae5a279f4738cfbae1790931a59
unknown <maxwellwong91@gmail.com>
2020-10-13 22:41:07 -0700

    stopping before moving onto firebase

--- server/routes/GetAllTodoByUser.py
+++ server/routes/GetAllTodoByUser.py
@@ -1,7 +1,19 @@
 from flask import request, Blueprint
+from ..database.database import session
+
 
 getAllTodoByUser = Blueprint("getAllTodoByUser", __name__)
 
-@getAllTodoByUser.route("/api/user/<id>/todo", methods=["GET"])
-def getAllTodoByUserRoute(id):
-  return {"todo" : ["todo1", "todo2", "todo3"]}
+@getAllTodoByUser.route("/api/user/<userId>/todo", methods=["GET"])
+def getAllTodoByUserRoute(userId):
+  if not userId:
+    return (jsonify({"error": "Missing userId"}), 400)
+
+  query = (f"SELECT * FROM todo.UserTodo WHERE userId = {userId}")
+
+  results = session.execute(query)
+  print (list(results))
+  # colNames = results._fields
+  # print (colNames)
+  # print (str(results))
+  return {"todo" : "tmp"}
