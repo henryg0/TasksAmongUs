6bc735f2077af66fdb9ada2f66c17a11235ddac8
henryg0 <hgip@uci.edu>
2020-10-21 00:00:49 -0700

    update todo

--- server/routes/GetAllFriends.py
+++ server/routes/GetAllFriends.py
@@ -1,7 +1,6 @@
 from flask import request, Blueprint, jsonify
 from ..database.database import db
 
-
 getAllFriends = Blueprint("getAllFriends", __name__)
 
 @getAllFriends.route("/api/user/<userId>/friend", methods = ["GET"])
@@ -9,7 +8,6 @@ def getAllFriendsRoute(userId):
   # add a checking method where only user can accept their own todo
   if not userId:
     return (jsonify({"error": "Missing requestId"}), 400)
-
 
   docs = db.collection('Friends')
   result = docs.where("userId", "==", userId).stream()
--- server/routes/UpdateTodo.py
+++ server/routes/UpdateTodo.py
@@ -1,15 +1,51 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, jsonify
+from ..database.database import db
 
 updateTodo = Blueprint("updateTodo", __name__)
 
-@updateTodo.route("/api/user/<id>/todo/<todo_id>/update", methods = ["PATCH"])
-def updateTodoRoute(id, todo_id):
+@updateTodo.route("/api/user/<userId>/todo/<todoId>/update", methods = ["PATCH"])
+def updateTodoRoute(userId, todoId):
+  if not userId:
+    return (jsonify({"error": "Missing userId"}), 400)
+    
+  if not todoId:
+    return (jsonify({"error": "Missing todoId"}), 400)    
+  
   data = request.get_json()
-  name = data.get("name")
+  
+  if not data:
+    return (jsonify({"error": "Missing data"}), 400)
+
+  todoName = data.get("todoName")
   description = data.get("description")
   deadline = data.get("deadline")
-  color = data.get("color")
+  imageUrl = data.get("imageUrl")
+  status = data.get("status")
 
-  return {"msg": "Todo updated"}
+  dataLog = {
+    "todoName":todoName,
+    "deadline":deadline,
+    "imageUrl":imageUrl,
+    "description":description,
+    "status":status
+  }
 
-    
+  for key in dataLog:
+    if not dataLog[key]:
+      return (jsonify({"error": "Missing {}".format(key)}), 400)
+  
+  doc = db.collection("Todo").document(todoId).get()
+  
+  if not doc.exists:
+    return (jsonify({"error": "Todo does not exist"}), 404)
+
+  docDict = doc.to_dict()
+  if not (docDict.get("userId") and docDict["userId"] == userId):
+    return (jsonify({"error": "User is not allowed to update this field"}), 403)
+
+  for key in dataLog:
+    docDict[key] = dataLog[key]
+
+  db.collection("Todo").document(todoId).set(docDict)
+      
+  return {"todo": docDict}
