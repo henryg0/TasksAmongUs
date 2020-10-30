c1ca0472d89f885ef5caf95b0ed64ae7be799c87
vincentktieu@ucsb.edu <vincentktieu@ucsb.edu>
2020-10-29 20:55:07 -0700

    dummy

--- client/src/utils/init.auth.js
+++ client/src/utils/init.auth.js
@@ -1,7 +1,7 @@
 export default function InitAuth() {
   window.gapi.load('auth2', () => {
     window.gapi.auth2.init({
-      client_id: process.env.REACT_APP_AUTH_CLIENT_ID
+      client_id: process.env.REACT_APP_AUTH_CLIENT_ID 
     }).then(() => {
       const authInstance =  window.gapi.auth2.getAuthInstance();
       const isSignedIn = authInstance.isSignedIn.get();
